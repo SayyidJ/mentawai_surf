914bb1f08c2bd27798a02a4080c3173dbc3fd22b
SayyidJ <sayyidjee@gmail.com>
2024-01-27 12:38:10 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: [
+    "./public/**/*.{html,js}",
+    "./node_modules/@fortawesome/**/*.js",
+    "./node_modules/@fortawesome/**/*.jsx",
+    "./node_modules/@fortawesome/**/*.ts",
+    "./node_modules/@fortawesome/**/*.tsx",
+  ],
   theme: {
     extend: {
       fontFamily: {
-        "inter": ['Inter', 'sans-serif'],
-        "krona": ['Krona One', 'sans-serif'],
-        "lato": ['Lato', 'sans-serif'],
-      }
+        inter: ["Inter", "sans-serif"],
+        krona: ["Krona One", "sans-serif"],
+        lato: ["Lato", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
